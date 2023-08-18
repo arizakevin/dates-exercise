1f0cd1ea4c1eb22b7d54a931a0dd2c08aa81d955
Kevin Williams <kevin.williams@buildertrend.com>
2023-08-18 10:59:20 -0500

    Updated code and tests for considering edge cases

--- main.test.ts
+++ main.test.ts
@@ -1,4 +1,4 @@
-import { timeToMinutes, availableSlotsForDay, totalAvailableSlots } from './main';
+import { timeToMinutes, availableSlotsForDay, totalAvailableSlots, toSortedAppointments } from './main';
 import { weekAppointments } from './data';
 
 describe('timeToMinutes', () => {
@@ -22,5 +22,75 @@ describe('availableSlotsForDay', () => {
 describe('totalAvailableSlots', () => {
 	it('should calculate total available 30-minute slots for a week', () => {
 		expect(totalAvailableSlots(weekAppointments)).toBeGreaterThan(0);
+	});
+});
+
+describe('timeToMinutes Edge Cases', () => {
+	it('should return null for invalid time format', () => {
+		expect(timeToMinutes("25:00")).toBeNull();
+		expect(timeToMinutes("12:70")).toBeNull();
+		expect(timeToMinutes("invalid")).toBeNull();
+	});
+});
+
+describe('availableSlotsForDay Edge Cases', () => {
+	it('should return 16 slots for a day with no appointments', () => {
+		expect(availableSlotsForDay([])).toBe(16);
+	});
+
+	it('should not count appointments outside of 9:00 to 17:00', () => {
+		const appointments = [
+			{ start: "8:00", duration: 60 },
+			{ start: "17:00", duration: 60 }
+		];
+		expect(availableSlotsForDay(appointments)).toBe(16);
+	});
+
+	it('should handle overlapping appointments', () => {
+			const appointments = [
+				{ start: "9:00", duration: 30 },
+				{ start: "9:15", duration: 30 }
+			];
+			// This should ideally throw an error or return a special value
+			// For this test, we'll assume overlapping appointments are invalid and should return null
+			expect(availableSlotsForDay(appointments)).toBeNull();
+	});
+});
+
+describe('totalAvailableSlots Edge Cases', () => {
+	it('should return 80 slots for a week with no appointments', () => {
+		expect(totalAvailableSlots([[], [], [], [], []])).toBe(80); // 5 days * 16 slots/day
+	});
+
+	it('should return 0 slots for a week fully booked', () => {
+		const fullyBookedDay = [
+			{ start: "9:00", duration: 480 } // 8 hours
+		];
+		expect(totalAvailableSlots([fullyBookedDay, fullyBookedDay, fullyBookedDay, fullyBookedDay, fullyBookedDay])).toBe(0);
+	});
+});
+
+describe('toSortedAppointments', () => {
+	it('should sort appointments based on start time', () => {
+		const appointments = [
+			{ start: "11:00", duration: 30 },
+			{ start: "9:00", duration: 60 },
+			{ start: "10:00", duration: 90 }
+		];
+		const sorted = toSortedAppointments(appointments);
+		expect(sorted[0].start).toBe("9:00");
+		expect(sorted[1].start).toBe("10:00");
+		expect(sorted[2].start).toBe("11:00");
+	});
+
+	it('should handle invalid time formats', () => {
+		const appointments = [
+			{ start: "invalid", duration: 30 },
+			{ start: "9:00", duration: 60 }
+		];
+		const sorted = toSortedAppointments(appointments);
+		// Assuming invalid times are treated as 0 minutes, they'll be at the start of the sorted array
+		expect(sorted[0].start).toBe("invalid");
+		expect(sorted[1].start).toBe("9:00");
 	});
 });
--- main.ts
+++ main.ts
@@ -1,31 +1,71 @@
 import { Appointment, weekAppointments } from './data';
 
-export function timeToMinutes(time: string): number {
+export function timeToMinutes(time: string): number | null {
 	const [hour, minute] = time.split(':').map(Number);
+	if (isNaN(hour) || isNaN(minute) || hour < 0 || hour >= 24 || minute < 0 || minute >= 60) {
+			return null;
+	}
 	return hour * 60 + minute;
 }
 
-export function availableSlotsForDay(appointments: Appointment[]): number {
-	let availableSlots = 0;
-	let lastEndTime = timeToMinutes("9:00"); // start of the day
 
-	appointments.forEach(appointment => {
+export function availableSlotsForDay(appointments: Appointment[]): number | null {
+	let availableSlots = 0;
+	let lastEndTime = timeToMinutes("9:00");
+
+	if (lastEndTime === null) {
+		return null; // Handle the potential null value
+	}
+
+	for (const appointment of appointments) {
 		const startTime = timeToMinutes(appointment.start);
+		if (!startTime) return null; // Invalid start time
+
+		const endTime = startTime + appointment.duration;
+
+		// Check if appointment is outside of 9:00 to 17:00
+		const dayStart = timeToMinutes("9:00");
+		const dayEnd = timeToMinutes("17:00");
+		if (dayStart && dayEnd && (startTime < dayStart || endTime > dayEnd)) {
+			continue; // Skip this appointment
+		}
+
+		// Check for overlapping appointments
+		if (startTime < lastEndTime) return null;
+
 		availableSlots += (startTime - lastEndTime) / 30;
-		lastEndTime = startTime + appointment.duration;
-	});
+		lastEndTime = endTime;
+	}
+
+	const endOfDay = timeToMinutes("17:00");
+	if (!endOfDay) {
+		return null; // Handle the potential null value
+	}
 
 	// Add slots available after the last appointment till 17:00
-	availableSlots += (timeToMinutes("17:00") - lastEndTime) / 30;
+	availableSlots += (endOfDay - lastEndTime) / 30;
 
 	return availableSlots;
 }
 
+export function toSortedAppointments(appointments: Appointment[]): Appointment[] {
+	return [...appointments].sort((a, b) => {
+		const timeA = timeToMinutes(a.start);
+		const timeB = timeToMinutes(b.start);
+
+		if (timeA === null || timeB === null) {
+			return 0;
+		}
+
+		return timeA - timeB;
+	});
+}
+
 export function totalAvailableSlots(weekAppointments: Appointment[][]): number {
 	return weekAppointments.reduce((total, dayAppointments) => {
-		// Sort appointments for the day based on start time
-		const sortedAppointments = dayAppointments.sort((a, b) => timeToMinutes(a.start) - timeToMinutes(b.start));
-		return total + availableSlotsForDay(sortedAppointments);
+		const sortedAppointments = toSortedAppointments(dayAppointments);
+		const availableForDay = availableSlotsForDay(sortedAppointments);
+		return total + (availableForDay ?? 0);
 	}, 0);
 }
 
